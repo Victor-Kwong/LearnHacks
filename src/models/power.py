07b816190e19bebc15c76c7d190592a8e30e3924
VictorK <victorkwong128@gmail.com>
2023-08-19 18:29:14 -0400

    2023-08-19

--- src/models/power.py
+++
@@ -1,8 +0,0 @@
-from models.base_model import BaseModel
-
-
-class Power(BaseModel):
-    def __init__(self, damage=0, difficulty=0, name=""):
-        self.damage = damage
-        self.difficulty = difficulty
-        self.name = name
